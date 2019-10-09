5f69b32c8597ce3706e25b63bbb2e3dff3fd5614
ssping <514079588@qq.com>
2019-10-09 15:48:02 +0800

    Login UI

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,36 @@
+import React, {Component} from 'react'
+import styles from './login.css'
 
-import styles from './login.css';
+import {Login} from 'ant-design-pro'
+import connect from 'dva'
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page login</h1>
-    </div>
-  );
+const {UserName,Password, Submit} = Login;
+
+
+export default class extends Component {
+  render(){
+    return (
+      <div className = {styles.loginForm}>
+        <img src="https://img.kaikeba.com/logo-new.png" className={styles.logo} alt=""/>
+        <Login>
+          <UserName
+            name="username"
+            placeholder="kaikeba"
+            rules={[{required:true,message: "input your user name "}]}
+          ></UserName>
+          <Password 
+          name="password"
+          placeholder="inpur your password wrod"
+          ></Password>
+          <Submit> Login </Submit>
+        </Login>
+
+
+      </div>
+
+    );
+  }
+
+
+
 }
